08a0d6a41965c8e719e89d52a354ec3ba542b67d
Max Knutsen <mknutsen99@gmail.com>
2020-12-19 11:08:48 -0800

    using math21 now, transpose and fitting on kalimba best i can

--- graphics.py
+++ graphics.py
@@ -1,189 +1,55 @@
+import subprocess
 from math import floor
+from threading import Thread
 from time import time
-from typing import List, Dict
+from typing import Dict, List
 
 import musicalbeeps
 import pygame
-
-from pygame import Color
-
+from mido import second2tick, bpm2tempo
 from mido.messages.messages import Message
-from mido import second2tick
+from music21.chord import Chord
+from music21.note import Note
+from music21.pitch import Pitch
+from music21.stream import Stream
+from pygame import Color
 from pygame.sprite import Sprite
 from pygame.surface import Surface
 from pygame.tests.draw_test import GREEN
 
-_DEFAULT_TEMPO = 200000
+_DESIRED_BPM = 40
+_TEMPO_MICROSECONDS_PER_BEAT = bpm2tempo(_DESIRED_BPM)
 _DEFAULT_TICKS_PER_BEAT = 480
 _KEY_VERITCAL_OFFSET = 0
 _KEY_HORIZONTAL_OFFSET = 100
 _KEY_WIDTH = 40
 _KEY_LENGTH_STEP = 60
 _KEY_MAX_LENGTH = 800
+_SECONDS_PER_MINUTE = 60
 _FONT_NAME = "Roboto-Bold.ttf"
 
-_midi_conversion_table: Dict[int, str] = {
-    127: "G9",
-    126: "F9#",
-    125: "F9",
-    124: "E9",
-    123: "D9#",
-    122: "D9",
-    121: "C9#",
-    120: "C9",
-    119: "B8",
-    118: "A8#",
-    117: "A8",
-    116: "G8#",
-    115: "G8",
-    114: "F8#",
-    113: "F8",
-    112: "E8",
-    111: "D8#",
-    110: "D8",
-    109: "C8#",
-    108: "C8",
-    107: "B7",
-    106: "A7#",
-    105: "A7",
-    104: "G7#",
-    103: "G7",
-    102: "F7#",
-    101: "F7",
-    100: "E7",
-    99: "D7#",
-    98: "D7",
-    97: "C7#",
-    96: "C7",
-    95: "B6",
-    94: "A6#",
-    93: "A6",
-    92: "G6#",
-    91: "G6",
-    90: "F6#",
-    89: "F6",
-    88: "E6",
-    87: "D6#",
-    86: "D6",
-    85: "C6#",
-    84: "C6",
-    83: "B5",
-    82: "A5#",
-    81: "A5",
-    80: "G5#",
-    79: "G5",
-    78: "F5#",
-    77: "F5",
-    76: "E5",
-    75: "D5#",
-    74: "D5",
-    73: "C5#",
-    72: "C5",
-    71: "B4",
-    70: "A4#",
-    69: "A4",
-    68: "G4#",
-    67: "G4",
-    66: "F4#",
-    65: "F4",
-    64: "E4",
-    63: "D4#",
-    62: "D4",
-    61: "C4#",
-    60: "C4",
-    59: "B3",
-    58: "A3#",
-    57: "A3",
-    56: "G3#",
-    55: "G3",
-    54: "F3#",
-    53: "F3",
-    52: "E3",
-    51: "D3#",
-    50: "D3",
-    49: "C3#",
-    48: "C3",
-    47: "B2",
-    46: "A2#",
-    45: "A2",
-    44: "G2#",
-    43: "G2",
-    42: "F2#",
-    41: "F2",
-    40: "E2",
-    39: "D2#",
-    38: "D2",
-    37: "C2#",
-    36: "C2",
-    35: "B1",
-    34: "A1#",
-    33: "A1",
-    32: "G1#",
-    31: "G1",
-    30: "F1#",
-    29: "F1",
-    28: "E1",
-    27: "D1#",
-    26: "D1",
-    25: "C1#",
-    24: "C1",
-    23: "B0",
-    22: "A0#",
-    21: "A0",
-    20: "&nbsp;",
-    19: "&nbsp;",
-    18: "&nbsp;",
-    17: "&nbsp;",
-    16: "&nbsp;",
-    15: "&nbsp;",
-    14: "&nbsp;",
-    13: "&nbsp;",
-    12: "&nbsp;",
-    11: "&nbsp;",
-    10: "&nbsp;",
-    9: "&nbsp;",
-    8: "&nbsp;",
-    7: "&nbsp;",
-    6: "&nbsp;",
-    5: "&nbsp;",
-    4: "&nbsp;",
-    3: "&nbsp;",
-    2: "&nbsp;",
-    1: "&nbsp;",
-    0: "&nbsp;", }
-
-_KEY_ORDERING = ["D3",
-                 "B2",
-                 "G2",
-                 "E2",
-                 "C2",
-                 "A1",
-                 "F1",
-                 "D1",
-                 "C1",
-                 "E1",
-                 "G1",
-                 "B1",
-                 "D2",
-                 "F2",
-                 "A2",
-                 "C3",
-                 "E3", ]
+_KEY_ORDERING = [Pitch("D3"),
+                 Pitch("B2"),
+                 Pitch("G2"),
+                 Pitch("E2"),
+                 Pitch("C2"),
+                 Pitch("A1"),
+                 Pitch("F1"),
+                 Pitch("D1"),
+                 Pitch("C1"),
+                 Pitch("E1"),
+                 Pitch("G1"),
+                 Pitch("B1"),
+                 Pitch("D2"),
+                 Pitch("F2"),
+                 Pitch("A2"),
+                 Pitch("C3"),
+                 Pitch("E3")]
 
 
-class Explosion(Sprite):
-    def __init__(self, pos):
-        Sprite.__init__(self)
-        self.image = Surface((60, 60))
-        self.rect = self.image.get_rect(center=pos)
-        self.state = 0
-
-    def update(self):
-        self.state += 1
-        self.image.fill((0, 0, 0))
-        pygame.draw.circle(self.image, (200, 5 * self.state, 0), self.image.get_rect().center, self.state)
-        if self.state > 30:
-            self.kill()
+def beats_to_seconds(beats_duration: int):
+    # print(_DESIRED_BPM, _SECONDS_PER_MINUTE, beats_duration)
+    return  _SECONDS_PER_MINUTE * beats_duration / _DESIRED_BPM
 
 
 class Key(Sprite):
@@ -191,7 +57,7 @@ class Key(Sprite):
         self.screen = screen
         Sprite.__init__(self)
         self.note = note
-        index = _KEY_ORDERING.index(note)
+        index = _KEY_ORDERING.index(Pitch(note))
         middle = floor(len(_KEY_ORDERING) / 2)
         self.distance_from_middle = abs(middle - index)
         self.key_length = _KEY_MAX_LENGTH - self.distance_from_middle * _KEY_LENGTH_STEP
@@ -200,76 +66,86 @@ class Key(Sprite):
         self.state = False
         # self.image.fill((self.distance_from_middle * 100 % 255, 60, 200))
         self.font = font
+        self.end_time_sec = 0
+
+    def press(self, duration: int) -> None:
+        self.state = True
+        self.end_time_sec = duration + floor(time())
 
     def update(self):
+        if self.state and self.end_time_sec < time():
+            self.state = False
+            self.end_time_sec = 0
+
         normal_color = (self.distance_from_middle * 100 % 255, 60, 200)
         highlight_color = (102, 255, 0)
         self.image.fill((highlight_color if self.state else normal_color))
-        index = _KEY_ORDERING.index(self.note)
+        index = _KEY_ORDERING.index(Pitch(self.note))
         img1 = self.font.render(self.note, False, GREEN)
         self.screen.blit(img1, (_KEY_HORIZONTAL_OFFSET + _KEY_WIDTH * index, 50))
         pass
 
 
-class Ball(Sprite):
-    def __init__(self, pos, screen):
-        Sprite.__init__(self)
-        self.image = Surface((40, 40))
-        self.image.fill((60, 60, 200))
-        self.rect = self.image.get_rect(center=pos)
-        self.dir = 3
-        self.screen = screen
-
-    def update(self):
-        if not self.screen.get_rect().contains(self.rect):
-            self.dir *= -1
-        self.image.fill((self.rect.x % 255, 60, 200))
-        self.rect.move_ip(self.dir, 0)
-
-
-def main(notes: List[Message]):
+def main(notes: Stream, lowest_note: Pitch, highest_note: Pitch):
     player = musicalbeeps.Player(volume=0.3,
                                  mute_output=False)
-    print(notes)
+    notes_list = list(notes)
+
+    lowest_note_midi = 130
+    for key in _KEY_ORDERING:  # try highest and see which covers more
+        if lowest_note.name == key.name:
+            lowest_note_midi = min(key.midi, lowest_note_midi)
+    adjustment = lowest_note.midi - lowest_note_midi
+    print(f"midi adjustment to fit on kalimba {adjustment}")
+
     pygame.init()
     screen = pygame.display.set_mode((1000, 1000))
     _SYS_FONT = pygame.font.SysFont("helvetica", floor(_KEY_WIDTH / 2))
-    key_list = [Key(key, screen, _SYS_FONT) for key in _KEY_ORDERING]
-    key_dict = {key.note: key for key in key_list}
+    key_list = [Key(str(key), screen, _SYS_FONT) for key in _KEY_ORDERING]
+    key_dict = {Pitch(key.note).midi: key for key in key_list}
     sprites = pygame.sprite.Group(key_list)
     clock = pygame.time.Clock()
     run = True
     begin_sec = time()
-    next_note: Message = notes.pop(0)
-    last_played = None
+    next_note = notes_list.pop(0)
     while run:
         # INSIDE OF THE GAME LOOP
+        if not notes_list:
+            begin_sec = time()
+            notes_list = list(notes)
+            next_note = notes_list.pop(0)
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 run = False
-            if e.type == pygame.MOUSEBUTTONDOWN:
-                sprites.add(Explosion(e.pos))
 
         screen.fill((0, 0, 0))
-        if notes:
+        if next_note:
             now_sec = time()
             time_since_begin_sec = now_sec - begin_sec
-            tics = second2tick(time_since_begin_sec, _DEFAULT_TICKS_PER_BEAT, _DEFAULT_TEMPO)
-            if next_note.time <= tics:
-                player.play_note(_midi_conversion_table[next_note.note], 0.5)
-                try:
-                    note_number = next_note.note - 12 * 3
-                    note_letter = _midi_conversion_table[note_number]
-                    if last_played:
-                        last_played.state = False
-                    last_played = key_dict[note_letter]
-                    last_played.state = True
-                except Exception as e:
-                    print(e)
-                begin_sec = time()
-                next_note = notes.pop(0)
+
+            tics = second2tick(time_since_begin_sec, _DEFAULT_TICKS_PER_BEAT, _TEMPO_MICROSECONDS_PER_BEAT)
+            # print(tics, next_note.midiTickStart)
+            while next_note and next_note.midiTickStart <= tics:
+                duration_seconds = beats_to_seconds(next_note.duration.quarterLength)
+                print("hey", next_note, next_note.duration)
+                pitches = []
+                if isinstance(next_note, Note):
+                    pitches = [next_note.pitch, ]
+
+                if isinstance(next_note, Chord):
+                    pitches = next_note.pitches
+
+                for note in pitches:
+                    Thread(target=player.play_note, args=[str(note), duration_seconds]).start()
+                    try:
+                        key_dict[note.midi - adjustment].press(
+                            duration_seconds)
+                    except Exception as e:
+                        print(note, note.midi, adjustment, e)
+                # begin_sec = time()
+                next_note = notes_list.pop(0) if notes_list else None
 
         sprites.draw(screen)
         sprites.update()
         pygame.display.flip()
-        clock.tick(20)
+        clock.tick()
--- main.py
+++ main.py
@@ -1,22 +1,71 @@
-from typing import Dict
+import os
+import random
 
-import mido
+from music21 import converter
+from music21.chord import Chord
+from music21.note import Note
+from music21.pitch import Pitch
+from music21.tree.trees import ElementTree
 
 import graphics
+from transpose import transpose
 
-_IMAGE_PATH = "/Users/mknutsen/Documents/GitHub/midiviz/pic.png"
-_SONG_FILE_PATH = "/Users/mknutsen/Library/Mobile Documents/com~apple~CloudDocs/ableton_workspace/sample/Wii Channels - Mii Channel.mid"
-_NOTE_ON = "note_on"
-mid = mido.MidiFile(_SONG_FILE_PATH)
+_SONG_DIR_PATH = "/Users/mknutsen/Library/Mobile Documents/com~apple~CloudDocs/ableton_workspace/sample/midi scales"
+_SONG_TITLES = ['Phrygian_dominant_scale_on_C.mid', 'Ionian_mode_C.mid', 'Harmonic_major_scale_C.mid',
+                'Blues_scale_common.mid', 'Hirajoshi_scale_on_C_Kostka-Payne_%26_Speed.mid',
+                'Double_harmonic_scale_quarter_tones.mid', 'Diatonic_scale_on_C_sopranino_clef.mid',
+                'Symmetrical_four-semitone_tritone_scale_on_C.mid', 'Enigmatic_scale_on_C_descending.mid',
+                'Minor_scale_on_C.mid', 'Diatonic_scale_on_C_tenuto.mid', 'Half_diminished_scale_C.mid',
+                'Harmonic_minor_on_C.mid', 'Locrian_mode_C.mid', 'Neapolitan_minor_scale_on_C.mid',
+                'Insen_scale_on_C.mid', 'Slendro_on_C.mid', 'Ukrainian_Dorian_mode_on_C.mid',
+                'Double_harmonic_scale.mid', 'Diatonic_scale_on_C_legato.mid', 'Gypsy_minor_scale.mid',
+                'Chromatic_scale_ascending_on_C.mid', 'Pythagorean_diatonic_scale_on_C.mid', 'C_major_scale.mid',
+                'Vietnamese_scale_of_harmonics_on_C.mid', 'Prometheus_scale_on_C.mid',
+                'Two-semitone_tritone_scale_on_C.mid', 'Phrygian_mode_C.mid.mp3',
+                'Hirajoshi_scale_on_C_Sachs_%26_Slonimsky.mid', 'Lydian_dominant_C.mid', 'Ionian.MID',
+                'Augmented_scale_on_C.mid', 'Chromatic_scale_descending_on_C.mid', 'Double_harmonic_scale_on_C.mid',
+                'C_harmonic_minor_scale_ascending_and_descending.mid', 'Bebop_dominant_scale_on_C.mid',
+                'Lydian_mode_C_midi.mid', 'C_minor_pentatonic_scale.mid', 'Hungarian_gypsy_scale_C.mid',
+                'Diatonic_scale_on_C_staccatissimo.mid', 'Diatonic_scale_on_C_marcato.mid',
+                'Diatonic_scale_on_C_bass_clef.mid', 'Neapolitan_major_scale_on_C.mid',
+                'Slendro_vs_whole_tone_scale_on_C.mid', '13-limit_just_decatonic_scale_on_C.mid',
+                'Tritone_scale_on_C.mid', 'Lydian_augmented_scale_on_C.mid', 'Aeolian_mode_C.mid',
+                'Diatonic_scale_on_C.mid', 'Major_bebop_scale_on_C.mid', 'Hirajoshi_scale_on_C_Burrows.mid',
+                'Istrian_mode_on_C.mid', 'Diatonic_scale_on_C_tablature.mid', 'Major_scale_on_C.mid',
+                'Mixolydian_mode_C.mid', 'Thai_pentatonic_scale_mode_1.mid', 'Major_locrian_C.mid',
+                'C-natural_major.mid', 'Hungarian_major_scale_on_C.mid', 'Persian_scale_on_C.mid', 'Algerian_scale.mid',
+                'Scale_of_harmonics_on_C.mid', 'Altered_scale_on_C.mid', 'Diatonic_scale_on_C_staccato.mid',
+                'Enigmatic_scale_on_C.mid', 'Octatonic_scales_on_C.mid', 'Melodic_minor_ascending_on_C.mid']
 
-# print(mid.tracks)
+keyDetune = []
+for i in range(0, 127):
+    keyDetune.append(random.randint(-30, 30))
+song_file_path = os.path.join(_SONG_DIR_PATH, _SONG_TITLES[random.randint(0, len(_SONG_TITLES))])
+print(song_file_path)
+b = converter.parse(song_file_path)
+key = b.analyze('key')  # correlatin coefficient
+print("before transpose")
+print(f"tonic.name: {key.tonic.name}, mode:{key.mode}, correlationCoefficient:{key.correlationCoefficient}")
+b = transpose(b)
+key = b.analyze('key')
+print("after transpose")
+print(f"tonic.name: {key.tonic.name}, mode:{key.mode}, correlationCoefficient:{key.correlationCoefficient}")
+tree: ElementTree = b.asTree(flatten=True, classList=[Note, Chord])
+lowest_note = Pitch("C8")
+highest_note = Pitch("C0")
+for x in tree:
+    pitches = []
+    if isinstance(x, Note):
+        pitches = [x.pitch, ]
+        pass
+    if isinstance(x, Chord):
+        pitches = x.pitches
 
-notes_in_song = [note for note in mid.tracks[1] if note.type == _NOTE_ON]
-highest = 0
-lowest = 99999999999999
-for note in notes_in_song:
-    lowest = min(note.note, lowest)
-    highest = max(note.note, highest)
+    for note in pitches:
+        if note < lowest_note:
+            lowest_note = note
+        if note > highest_note:
+            highest_note = note
+print(f"lowest_note: {lowest_note}, lowest_note: {highest_note}")
 
-print(lowest, highest)
-graphics.main(notes_in_song)
+graphics.main(tree, lowest_note, highest_note)
